f154fb6efc24f4c245694fc8a48c87679e05bcf5
Ahmad Sadeddin <asadeddin@gmail.com>
2025-02-18 21:13:56 -0800

    Corgea's suggested fix for issue 52997975-219e-46f0-9b22-5f8882dec747

--- routes/createProductReviews.ts
+++ routes/createProductReviews.ts
@@ -16,10 +16,12 @@ module.exports = function productReviews () {
   return (req: Request, res: Response) => {
     const user = security.authenticatedUsers.from(req)
     challengeUtils.solveIf(challenges.forgedReviewChallenge, () => { return user && user.data.email !== req.body.author })
+    const sanitizedMessage = validator.escape(req.body.message);
+    const sanitizedAuthor = validator.escape(req.body.author);
     reviews.insert({
       product: req.params.id,
-      message: req.body.message,
-      author: req.body.author,
+      message: sanitizedMessage,
+      author: sanitizedAuthor,
       likesCount: 0,
       likedBy: []
     }).then(() => {
